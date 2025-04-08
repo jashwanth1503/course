8aad9ace4595246b5f5b654613f436621e7760b3
jashwanth Kumar <jashwanthkumar@jashwanths-MacBook-Air.local>
2025-04-08 23:56:27 +0530

    class1-flexbox

--- App.js
+++ App.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function FlexLayoutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
+      <View style={styles.box1}>
+        <Text>Box 1</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text>Box 2</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text>Box 3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column', 
+    justifyContent: 'space-around',
+    alignItems: 'center', 
+    flex:1, 
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
+  box1: {
+    backgroundColor: '#008080',
+    padding: 20,
+    height:100,
+    width:'80%',
+    alignItems:'center',
+    shadowColor: 'black',
+    shadowOffset:{width:2,height:4},
+    shadowOpacity:0.5,
+    borderRadius:4,
+    justifyContent:'center',
+    elevation:5
+    
+  },
+  box2: {
+    backgroundColor: '#5F6A6A',
+    padding: 20,
+    height:100,
+    width:'80%',
+    alignItems:'center',
+    hadowColor: 'black',
+    shadowOffset:{width:2,height:4},
+    shadowOpacity:0.5,
+    borderRadius:4,
+    justifyContent:'center',
+    elevation:5
+  },
+  box3: {
+    backgroundColor: '#D7DBDD',
+    padding: 20,
+    height:100,
+    width:'80%',
+    alignItems:'center',
+    hadowColor: 'black',
+    shadowOffset:{width:2,height:4},
+    shadowOpacity:0.5,
+    borderRadius:4,
+    justifyContent:'center',
+    elevation:5
   },
 });
